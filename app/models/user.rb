497b3442c17ac8ddc4da0da0e785b464105b3780
christos5 <christos@factory39.io>
2020-08-04 17:55:22 +0300

    add

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
-    
+    REGEX_PATTERN = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :username, presence: true, 
+                         uniqueness: { case_sensitive: false }, 
+                         length: {minimum: 3, maximum: 25}
+    validates :email, presence: true, 
+                      length: {maximum: 105},
+                      format: { with: REGEX_PATTERN}
 end
