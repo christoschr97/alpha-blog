5b69a927c458e45b38c05bd9f4d7d0aa3a2a95a8
christos5 <christos@factory39.io>
2020-08-05 10:07:40 +0300

    adding signup form

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   # resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]
   resources :articles # all the restful routes for our articles resources
   get 'signup', to: 'users#new'
+  # post 'users', to: 'users#create'
+  resources :users, except: [:new]
 end
